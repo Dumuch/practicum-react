b20f840ff2d189207b40999b00fce92268e9753e
kizin_d <dumuch@yandex.ru>
2024-05-20 08:53:50 +0800

    Создан app-header

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,40 @@
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './styles.module.css';
+
+const AppHeader = () => {
+    return (
+        <header>
+            <div className="container">
+                <nav className={`${styles.navigation} d-flex justify-between`}>
+                    <div className={'d-flex justify-between'}>
+
+                        <div className={'d-flex justify-between pl-5 pr-5 pt-4 pb-4 mr-2'}>
+                            <div className="pr-2">
+                                <BurgerIcon type="primary" />
+                            </div>
+                            <span className={`${styles.title} text text_type_main-medium`}>Конструктор</span>
+                        </div>
+
+                        <div className={'d-flex justify-between pl-5 pr-5 pt-4 pb-4'}>
+                            <div className="pr-2">
+                                <ListIcon type="secondary" />
+                            </div>
+                            <span className={`${styles.title} text text_type_main-medium text_color_inactive`}>Лента заказов</span>
+                        </div>
+                    </div>
+                    <Logo />
+                    <div className={'d-flex justify-between pl-5 pr-5 pt-4 pb-4'}>
+                        <div className="pr-2">
+                            <ProfileIcon type="secondary" />
+                        </div>
+                        <span
+                            className={`${styles.title} text text_type_main-medium text_color_inactive`}>Личный кабинет</span>
+                    </div>
+
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default AppHeader
