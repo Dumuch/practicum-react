8ab80ed00db327bf2a590bd79350a2bdcca52fb9
kizin_d <dumuch@yandex.ru>
2024-05-22 14:32:44 +0800

    Добавлена загрузка данных с api

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,34 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styles from './styles.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import SubmitOrder from '../submit-order/submit-order';
-import { ingredients } from '../../utils/mock-data';
 import { randomInt } from '../../helpers';
+import { IIngredient } from '../../models/common';
+import { API } from '../../constants';
 
 function App() {
+    const [ingredients, setIngredients] = useState<IIngredient[]>([])
+    const [error, setError] = useState<string | null>(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch(API.ingredients).then(r => r.json()).catch(e => {
+            setError('Сервер не доступен')
+        }).then(({ data, success }) => {
+            if (success) {
+                setIngredients(data)
+            } else {
+                setError('Сервер не доступен')
+            }
+        }).finally(() =>{
+            setLoading(false)
+        })
+    }, []);
+
+
     const buns = useMemo(() => {
         return ingredients.filter((item) => item.type === 'bun')
     }, [ingredients])
@@ -20,20 +41,33 @@ function App() {
         return ingredients.filter((item) => item.type === 'sauce')
     }, [ingredients])
 
-    const applyIngredients = mains.slice(randomInt(1,mains.length - 1),randomInt(1,mains.length - 1)).concat(sauces.slice(randomInt(1,sauces.length - 1),randomInt(1,sauces.length - 1)))
-    const applyBun = buns[randomInt(0,buns.length - 1)]
+
+    const applyIngredients = mains.slice(randomInt(1, mains.length - 1), randomInt(1, mains.length - 1)).concat(sauces.slice(randomInt(1, sauces.length - 1), randomInt(1, sauces.length - 1)))
+    const applyBun: IIngredient | undefined = buns[randomInt(0, buns.length - 1)]
 
     return (
         <div className={`${styles.app} d-flex flex-column`}>
             <AppHeader />
             <main className={`${styles.container} container d-flex justify-between`}>
-                <div className={`${styles.column} mr-10`}>
-                    <BurgerIngredients mains={mains} buns={buns} sauces={sauces} />
-                </div>
-                <div className={styles.column}>
-                    <BurgerConstructor applyIngredients={applyIngredients} applyBun={applyBun} />
-                    <SubmitOrder applyIngredients={applyIngredients} applyBun={applyBun} />
-                </div>
+                {loading ? (
+                    <p>Загрузка ингредиентов...</p>
+                ) : (
+                    <>
+                        {error ? (
+                            <p>{error}</p>
+                        ) : (
+                            <>
+                                <div className={`${styles.column} mr-10`}>
+                                    <BurgerIngredients mains={mains} buns={buns} sauces={sauces} />
+                                </div>
+                                <div className={styles.column}>
+                                    <BurgerConstructor applyIngredients={applyIngredients} applyBun={applyBun} />
+                                    <SubmitOrder applyIngredients={applyIngredients} applyBun={applyBun} />
+                                </div>
+                            </>
+                        )}
+                    </>
+                )}
             </main>
 
         </div>
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const API = {
+    ingredients: 'https://norma.nomoreparties.space/api/ingredients'
+}
