bf03240ef0876f89487ba40bca97cb41fd6ff02d
kizin_d <dumuch@yandex.ru>
2024-06-04 14:39:24 +0800

    Добавлено отслеживание скролла

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo, useState } from 'react';
+import React, { FC, LegacyRef, UIEventHandler, useCallback, useMemo, useRef, useState } from 'react';
 import styles from './styles.module.css'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import IngredientCard from '../ingredient-card/ingredient-card';
@@ -15,11 +15,17 @@ interface BurgerIngredientsProps {
     ingredients: IIngredient[]
 }
 
+const OFFSET_Y = 40
+
 const BurgerIngredients: FC<BurgerIngredientsProps> = ({ ingredients }) => {
     const [current, setCurrent] = React.useState('one')
 
     const {currentIngredient} = useSelector((state: RootState) => state.commonStore)
     const dispatch = useDispatch<AppDispatch>()
+
+    const oneTabRef = useRef<HTMLHeadingElement | null >(null)
+    const twoTabRef = useRef<HTMLHeadingElement | null >(null)
+    const threeTabRef = useRef<HTMLHeadingElement | null >(null)
 
     const { buns, sauces, mains } = useMemo(() => {
         return groupIngredients(ingredients)
@@ -31,8 +37,19 @@ const BurgerIngredients: FC<BurgerIngredientsProps> = ({ ingredients }) => {
 
     const closeModal = () => {
         dispatch(setCurrentIngredient(null))
-
     }
+
+    const scrollHandler = useCallback((e: React.UIEvent<HTMLDivElement>) => {
+        const pageY = e.currentTarget.getBoundingClientRect().top
+
+        if(pageY > (threeTabRef.current?.getBoundingClientRect().top ?? 0) - OFFSET_Y) {
+            setCurrent('three')
+        } else if(pageY > (twoTabRef.current?.getBoundingClientRect().top ?? 0) - OFFSET_Y){
+            setCurrent('two')
+        } else {
+            setCurrent('one')
+        }
+    },[])
 
     return (
         <section className={styles.wrapperBurgerConstructor}>
@@ -48,8 +65,8 @@ const BurgerIngredients: FC<BurgerIngredientsProps> = ({ ingredients }) => {
                     Начинки
                 </Tab>
             </div>
-            <div className={styles.wrapperIngredients}>
-                <h3 className={'text text_type_main-medium'}>Булки</h3>
+            <div className={styles.wrapperIngredients} onScroll={scrollHandler}>
+                <h3 className={'text text_type_main-medium'} ref={oneTabRef}>Булки</h3>
                 <ul className={`${styles.list} d-flex flex-wrap mb-10 mt-6 pl-4 pr-4`}>
                     {buns.map(ingredient => {
                         return (
@@ -60,7 +77,7 @@ const BurgerIngredients: FC<BurgerIngredientsProps> = ({ ingredients }) => {
                     })}
                 </ul>
 
-                <h3 className={'text text_type_main-medium'}>Соусы</h3>
+                <h3 className={'text text_type_main-medium'} ref={twoTabRef}>Соусы</h3>
                 <ul className={`${styles.list} d-flex flex-wrap mb-10 mt-6 pl-4 pr-4`}>
                     {sauces.map(ingredient => {
                         return (
@@ -73,7 +90,7 @@ const BurgerIngredients: FC<BurgerIngredientsProps> = ({ ingredients }) => {
                 </ul>
 
 
-                <h3 className={'text text_type_main-medium'}>Начинки</h3>
+                <h3 className={'text text_type_main-medium'} ref={threeTabRef}>Начинки</h3>
                 <ul className={`${styles.list} d-flex flex-wrap mb-10 mt-6 pl-4 pr-4`}>
                     {mains.map(ingredient => {
                         return (
