bc473807a77450f179fc745a8f0ac92ecfc6e8eb
Dumuch <>
2024-06-20 10:54:06 +0800

    Add react-router

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -8,11 +8,11 @@ const AppHeader = () => {
             <div className="container">
                 <nav className={`${styles.navigation} d-flex justify-between pt-4 pb-4`}>
                     <div className={'d-flex justify-between'}>
-                        <Link title={'Конструктор'} icon={<BurgerIcon type="primary" />} />
-                        <Link title={'Лента заказов'} icon={<ListIcon type="secondary" />} disabled={true} />
+                        <Link title={'Конструктор'} icon={<BurgerIcon type="primary" />} href={'/'} />
+                        <Link title={'Лента заказов'} icon={<ListIcon type="secondary" />} disabled={true} href={'/'} />
                     </div>
                     <Logo />
-                    <Link title={'Личный кабинет'} icon={<ProfileIcon type="secondary" />} disabled={true} />
+                    <Link title={'Личный кабинет'} icon={<ProfileIcon type="secondary" />} disabled={true} href={'/'} />
                 </nav>
             </div>
         </header>
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export {default as MainPage} from './main'
+export {default as LoginPage} from './login'
+export {default as RegisterPage} from './register'
+export {default as ForgotPasswordPage} from './forgot-password'
+export {default as ResetPasswordPage} from './reset-password'
+export {default as ProfilePage} from './profile'
+export {default as IngredientsPage} from './ingredients'
+export {default as NotFound404Page} from './not-found'
